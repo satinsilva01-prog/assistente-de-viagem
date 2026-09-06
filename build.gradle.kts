df281918058cde3672b0cdece2370a9ffa7ce2b1
satinsilva01-prog <satinsilva01@gmail.com>
2026-09-05 23:52:53 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-android {
-    namespace="com.assistentedeviagem.app"
-    compileSdk=35
-    defaultConfig {
-        applicationId="com.assistentedeviagem.app"
-        minSdk=26
-        targetSdk=35
-        versionCode=2
-        versionName="2.0"
-    }
-}
-dependencies {
-    implementation("com.google.android.gms:play-services-location:21.3.0")
+    id("com.android.application") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
