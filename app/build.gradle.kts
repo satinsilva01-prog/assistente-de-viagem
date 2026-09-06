83523d51544f985940808ef54aaa877328a09eb4
satinsilva01-prog <satinsilva01@gmail.com>
2026-09-05 23:16:04 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.assistentedeviagem.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.assistentedeviagem.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+}
