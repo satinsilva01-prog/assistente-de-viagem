fe28a420f5460c7f187342a7870229a91a422f53
satinsilva01-prog <satinsilva01@gmail.com>
2026-09-19 13:22:11 -0300

    Bump version for corrected APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ kotlinOptions {
         applicationId = "com.assistentedeviagem.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 }
 
